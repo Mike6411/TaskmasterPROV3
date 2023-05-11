b3ba4bfe4711c3d92deb6c72798e36bd2551c837
Mike6411 <miquelnadalmudarra@gmail.com>
2023-05-11 13:56:25 +0200

    Ui de moment be

---
+++ app/src/main/java/com/example/taskmasterprov3/AddNote.kt
@@ -0,0 +1,11 @@
+package com.example.taskmasterprov3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddNote : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+    }
+}
